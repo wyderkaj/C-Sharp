ebcd3f3769ed8935ff0d61969b6f7945a525174a
wyderkaj <wyderka.j@gmail.com>
2022-10-20 09:30:22 +0200

    Add files via upload
    Game Rock Paper Scissors. You are about to play a game rock, paper, scissors against the computer.

---
+++ rock_paper_scissors_game.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace rock_paper_scissors_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+            String answer;
+
+            Console.WriteLine("Game rock paper scissors!");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("You are about to play a game rock, paper, scissors against the computer.");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Rock crushes scissors, scissors cut paper, and paper covers rock. See who wins each round!");
+            System.Threading.Thread.Sleep(1000);
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+                answer = "";
+
+                while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                {
+                    Console.WriteLine();
+                    Console.Write("Enter ROCK, PAPER or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();                  
+                }
+
+                switch (random.Next(1, 4))
+                {
+                    case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer);
+                Console.WriteLine();
+                System.Threading.Thread.Sleep(500);
+
+                switch (player)
+                {
+                    case "ROCK":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        break;
+                    case "PAPER":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        break;
+                    case "SCISSORS":
+                        if (computer == "ROCK")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        break; 
+                }
+
+                Console.WriteLine();
+                Console.Write("Would you like to play again (Y/N): ");
+                answer = Console.ReadLine();
+                answer = answer.ToUpper();
+
+                if (answer == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                }
+                 
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Thanks for playing!");
+
+            Console.ReadKey();
+        }
+    }
+}
